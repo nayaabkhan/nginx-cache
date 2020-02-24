4dd19fdc7d94b37b2dda0e42e41570fdf0088bb7
Nayaabkhan Khan <khannayaab@gmail.com>
2020-02-24 16:23:50 +0530

    Basic setup

---
+++ app/app.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+
+get '/*' do
+    out = "<h1>Current Time Is</h1>" + "\n"
+
+    # Get current time
+    now = Time.now
+    current_path = request.fullpath
+
+    out += '<p>'+ now.strftime("%Y.%d.%m %H:%M:%S") +'</p>' + "\n"
+    out += '<p>'+ current_path +'</p>' + "\n"
+
+    return out
+end
